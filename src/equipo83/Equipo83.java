51ac367bd97ab39a51818d001b58a65469295976
medchajari <141642941+medchajari@users.noreply.github.com>
2023-09-07 20:03:09 -0300

    Update Equipo83.java

--- src/equipo83/Equipo83.java
+++ src/equipo83/Equipo83.java
@@ -4,15 +4,10 @@
  */
 package equipo83;
 
-/**
- *
- * @author MeD Servicios Inf
- */
+
 public class Equipo83 {
 
-	/**
-	 * @param args the command line arguments
-	 */
+	
 	public static void main(String[] args) {
 		// TODO code application logic here
 	}
