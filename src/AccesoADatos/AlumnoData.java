84130cef9b42a8f5cdc0734fe5f0a57d95b17a1a
medchajari <141642941+medchajari@users.noreply.github.com>
2023-09-08 10:56:01 -0300

    clase alumno

--- src/AccesoADatos/AlumnoData.java
+++ src/AccesoADatos/AlumnoData.java
@@ -1,6 +1,7 @@
 
 package AccesoADatos;
 
+import Entidades.Alumno;
 import java.sql.Connection;
 
 
@@ -9,8 +10,13 @@ public class AlumnoData {
     private Connection con = null;
     
     //Construcctor
-    public AlumnoData(){
-        con=Conexion.getConexion();
+    public AlumnoData(){        
+        con=Conexion.getConexion();    
+    }
+    
+    public void guardarAlumno(Alumno alumno){
+    
+        String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaNacimiento, estado) VALUE(?,?,?,?,?)";
     
     }
     
