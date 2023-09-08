a1206fbdea094ed319abe45e647c019e584a4897
medchajari <141642941+medchajari@users.noreply.github.com>
2023-09-08 13:05:23 -0300

    guardar alumno

--- src/AccesoADatos/AlumnoData.java
+++ src/AccesoADatos/AlumnoData.java
@@ -3,20 +3,54 @@ package AccesoADatos;
 
 import Entidades.Alumno;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 
 
 public class AlumnoData {    
     
     private Connection con = null;
     
-    //Construcctor
+    //Construcctor de conexion
     public AlumnoData(){        
         con=Conexion.getConexion();    
     }
     
+    //metodo
     public void guardarAlumno(Alumno alumno){
     
         String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaNacimiento, estado) VALUE(?,?,?,?,?)";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNacimiento()));
+            ps.setBoolean(5, alumno.isActivo());
+            
+            //ejecutamos, enviamos los datos
+            ps.executeUpdate();
+            
+            //obtener la clave
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            //si se pudo agregar setamos el id
+            if(rs.next()){
+                
+                alumno.setIdAlumno(rs.getInt(1));
+                JOptionPane.showMessageDialog(null,"Alumno guardado exitosamente.");
+            
+            }
+            ps.close();
+            
+            
+            //si nos equivocamos en algun datos nos muestra este error
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al tratar de acceder a la tabla Alumno.");
+        }
     
     }
     
--- src/equipo83/Equipo83.java
+++ src/equipo83/Equipo83.java
@@ -1,8 +1,11 @@
 
 package equipo83;
 
+import AccesoADatos.AlumnoData;
 import AccesoADatos.Conexion;
+import Entidades.Alumno;
 import java.sql.Connection;
+import java.time.LocalDate;
 
 
 public class Equipo83 {
@@ -10,6 +13,10 @@ public class Equipo83 {
 	
 	public static void main(String[] args) {
 		Connection con=Conexion.getConexion();
+                
+                Alumno Juan=new Alumno(33011644,"Perez","Juan", LocalDate.of(1987, 5, 15), true);
+                AlumnoData alu = new AlumnoData();
+                alu.guardarAlumno(Juan);
 	}
 	
 }
