243e0708498d4b377a98a2ad33cf803f1c3f5d4b
medchajari <141642941+medchajari@users.noreply.github.com>
2023-09-12 10:21:09 -0300

    materia

--- src/AccesoADatos/InscripcionData.java
+++ src/AccesoADatos/InscripcionData.java
@@ -1,7 +1,48 @@
 
 package AccesoADatos;
 
+import Entidades.Materia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class InscripcionData {
+	
+	private Connection con = null;
+    
+    //Construcctor de conexion
+	public InscripcionData(){        
+        con=Conexion.getConexion();    
+    }
+    
+    
+	public List <Materia> obtenerMateriasCursadas(int id){
+		List<Materia> materias = new ArrayList<Materia>();
+		
+		try {
+			String sql = "SELECT inscripcion.idMateria, nombre, anio FROM inscripcion, materia WHERE inscripcion.idMateria = materia.idMateria AND inscripcion.idAlumno = ?;";
+			
+		
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			Materia materia;
+			while(rs.next()){
+			
+				materia = new Materia();
+				materia.setIdMateria(rs.getInt("idMateria"));
+				
+			
+			}
+			
+			
+		} catch (Exception e) {
+		}
+		
+	}
+    
     
 }
